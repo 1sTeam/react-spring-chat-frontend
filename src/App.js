05da7e4688fbc22a803e7414032b0ce29bb34118
pkh0106 <rudgml742@gmail.com>
2022-10-03 12:57:53 +0900

    Intro 회원 가입 버튼 기능 구현, 회원 가입 파일만 생성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Intro from "./routers/Intro";
 import Login from "./routers/Login";
+import SignUp from "./routers/SignUp";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Intro />}></Route>
         <Route path="login" element={<Login />}></Route>
+        <Route path="signUp" element={<SignUp />}></Route>
       </Routes>
     </div>
   );
--- src/routers/Intro.js
+++ src/routers/Intro.js
@@ -7,11 +7,15 @@ function Intro() {
     navigate("/login");
   };
 
+  const goSignUp = () => {
+    navigate("/signUp");
+  };
+
   return (
     <div>
       <button onClick={goLogin}>로그인하기</button>
       <br />
-      <button>회원 가입</button>
+      <button onClick={goSignUp}>회원 가입</button>
     </div>
   );
 }
