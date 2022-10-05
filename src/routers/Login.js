083b75ee66854a22c11af40aeba365dd50ab159f
pkh0106 <rudgml742@gmail.com>
2022-10-05 10:32:54 +0900

    [Feat] : useState 관련 피드백 수용 및 수정

--- src/routers/Login.js
+++ src/routers/Login.js
@@ -1,47 +1,51 @@
 import { useState } from "react";
 
 function Login() {
-  const [id, setID] = useState("");
-  const [password, setPassword] = useState("");
-
+  const [account, setAccount] = useState({ ID: "", password: "" });
   const [userPassword, setUserPassword] = useState("1234");
+
+  const onChangeState = (e) => {
+    setAccount({
+      ...account,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   const onSubmit = (e) => {
     e.preventDefault();
-    const getID = e.target.ID.value;
-    const getPassword = e.target.passWord.value;
-    if (getID === "" && getPassword === "") {
-      alert("아이디와 비밀번호를 입력하세요");
+    if (account.ID === "" || account.password === "") {
+      alert("아이디 또는 비밀번호를 입력하세요");
       return;
-    } else if (getID === "") {
-      alert("아이디를 입력하세요");
-      return;
-    } else if (getPassword === "") {
-      alert("비밀번호를 입력하세요");
-      return;
-    } else {
-      //모두 입력된 경우
-      setID(getID);
-      setPassword(getPassword);
     }
 
-    if (password !== userPassword) {
+    if (account.password !== userPassword) {
       alert("비밀번호가 틀렸습니다!!");
       e.target.passWord.value = "";
       return;
     }
 
-    console.log(id, password);
-    e.target.ID.value = "";
-    e.target.passWord.value = "";
+    console.log(account);
+    alert("성공");
   };
 
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input type="text" name="ID" placeholder="ID" />
+        <input
+          type="text"
+          name="ID"
+          placeholder="ID"
+          value={account.ID}
+          onChange={onChangeState}
+        />
         <br />
-        <input type="password" name="passWord" placeholder="PASSWORD" />
+        <input
+          type="password"
+          name="password"
+          placeholder="PASSWORD"
+          value={account.password}
+          onChange={onChangeState}
+        />
         <br />
         <input type="submit" value="Login" />
       </form>
