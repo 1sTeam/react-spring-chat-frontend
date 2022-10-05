c690d99bcce882e029280bb891edfdd649f384a6
pkh0106 <rudgml742@gmail.com>
2022-10-05 11:27:10 +0900

    [Feat] : 회원가입 폼 생성

--- src/routers/SignUp.js
+++ src/routers/SignUp.js
@@ -1,7 +1,30 @@
+import { useState } from "react";
+
 function SignUp() {
+  const [newAccount, setNewAccount] = useState({
+    email: "",
+    nickname: "",
+    ID: "",
+    pwd: "",
+  });
+  const [chkPwd, setchkPwd] = useState("");
+
   return (
     <div>
-      <h1>회원가입</h1>
+      <form>
+        <div>아이디</div>
+        <input type="text" name="ID" />
+        <div>비밀번호</div>
+        <input type="password" name="pwd" />
+        <div>비밀번호 재확인</div>
+        <input type="password" name="chkpassword" />
+        <div>닉네임</div>
+        <input type="text" name="nickname" />
+        <div>이메일</div>
+        <input type="text" name="email" />
+        <br />
+        <input type="submit" value="계정 생성" />
+      </form>
     </div>
   );
 }
