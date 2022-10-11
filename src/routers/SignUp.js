546f50b8608ed34d0ed5354e9dcc11e2da78cda8
pkh0106 <rudgml742@gmail.com>
2022-10-11 16:01:10 +0900

    [Feat] : 회원가입에 대한 기능 추가(정보 가져오기, 비밀번호 재확인)

--- src/routers/SignUp.js
+++ src/routers/SignUp.js
@@ -2,26 +2,47 @@ import { useState } from "react";
 
 function SignUp() {
   const [newAccount, setNewAccount] = useState({
-    email: "",
-    nickname: "",
     ID: "",
     pwd: "",
+    nickname: "",
+    email: "",
   });
   const [chkPwd, setchkPwd] = useState("");
 
+  const onChangeState = (e) => {
+    if (e.target.name === "chkpassword") {
+      setchkPwd(e.target.value);
+    } else {
+      setNewAccount({
+        ...newAccount,
+        [e.target.name]: e.target.value,
+      });
+    }
+  };
+
+  const onMakeAccount = (e) => {
+    e.preventDefault();
+    if (chkPwd !== newAccount.pwd) {
+      alert("비밀번호를 확인하세요");
+    } else {
+      //계정 생성
+      console.log(newAccount);
+    }
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={onMakeAccount}>
         <div>아이디</div>
-        <input type="text" name="ID" />
+        <input type="text" name="ID" onChange={onChangeState} />
         <div>비밀번호</div>
-        <input type="password" name="pwd" />
+        <input type="password" name="pwd" onChange={onChangeState} />
         <div>비밀번호 재확인</div>
-        <input type="password" name="chkpassword" />
+        <input type="password" name="chkpassword" onChange={onChangeState} />
         <div>닉네임</div>
-        <input type="text" name="nickname" />
+        <input type="text" name="nickname" onChange={onChangeState} />
         <div>이메일</div>
-        <input type="text" name="email" />
+        <input type="text" name="email" onChange={onChangeState} />
         <br />
         <input type="submit" value="계정 생성" />
       </form>
