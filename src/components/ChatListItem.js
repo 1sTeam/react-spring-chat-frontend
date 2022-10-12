0ff688fdb40f5ef24a562c623550a91b4a972c9c
pkh0106 <rudgml742@gmail.com>
2022-10-12 11:36:25 +0900

    [Feat] : 채팅 목록 item 클릭 시 채팅 방 이동 기능 구현

--- src/components/ChatListItem.js
+++ src/components/ChatListItem.js
@@ -1,8 +1,13 @@
 import "../css/ChatListItem.css";
+import { useNavigate } from "react-router-dom";
 
 function ChatListItem({ chatRoomName, currentTalk, isChecked }) {
+  const navigate = useNavigate();
+  const goChatRoom = () => {
+    navigate("/ChatRoom");
+  };
   return (
-    <div className="chatRoomInfo">
+    <div className="chatRoomInfo" onClick={goChatRoom}>
       <div>
         <div id="chatRoomName">{chatRoomName}</div>
         <div id="currentTalk">{currentTalk}</div>
