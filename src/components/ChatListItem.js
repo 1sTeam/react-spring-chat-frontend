bfb26e9dcb22690f58202d2470f9a76e4a8e4030
pkh0106 <rudgml742@gmail.com>
2022-11-04 08:29:06 +0900

    [Feat] : 필요 정보 수정

--- src/components/ChatListItem.js
+++ src/components/ChatListItem.js
@@ -2,7 +2,7 @@ import "../css/ChatListItem.css";
 import { useNavigate } from "react-router-dom";
 import { BsFillPersonFill } from "react-icons/bs";
 
-function ChatListItem({ chatRoomName, currentTalk, currentChat, isChecked }) {
+function ChatListItem({ chatRoomName }) {
   const navigate = useNavigate();
   const goChatRoom = () => {
     navigate("/ChatRoom");
@@ -12,12 +12,11 @@ function ChatListItem({ chatRoomName, currentTalk, currentChat, isChecked }) {
       <BsFillPersonFill className="chatPersonIcon" />
       <div className="chatRoomContext">
         <div className="chatRoomName">{chatRoomName}</div>
-        <div className="currentTalk">{currentTalk}</div>
       </div>
-      <div className="chatOption">
+      {/* <div className="chatOption">
         <div className="currentTime">{currentChat}</div>
         <div className={`alarm ${isChecked ? "checked" : ""}`}></div>
-      </div>
+      </div> */}
     </div>
   );
 }
