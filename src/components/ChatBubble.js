8beff2e90b57fe8424ee4cb3ed8320f7cd4bce28
pkh0106 <rudgml742@gmail.com>
2022-11-09 18:00:47 +0900

    [Feat] : 길이 별에 따른 말풍선 크기 변경

--- src/components/ChatBubble.js
+++ src/components/ChatBubble.js
@@ -2,7 +2,11 @@ import "../css/ChatBubble.css";
 
 function ChatBubble({ chatLog, nowUser }) {
   return (
-    <div className={`bubble ${chatLog.sender === nowUser ? "me" : "you"}`}>
+    <div
+      className={`bubble ${chatLog.sender === nowUser ? "me" : "you"} ${
+        chatLog.content.length > 6 ? "long" : "short"
+      }`}
+    >
       {chatLog.content}
       <div className="time">{chatLog.time}</div>
     </div>
